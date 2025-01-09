05c72a7850a91293e69b301a2d77d94229d18620
Maria Kulesza <mariamartakulesza@gmail.com>
2025-01-09 13:40:50 +0100

    add Composite

---
+++ MealMap.Application/Composite/IngredientComposite.cs
@@ -0,0 +1,41 @@
+﻿using MealMap.Domain.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealMap.Application.Composite
+{
+    public class IngredientComposite : IIngredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+
+        private List<IIngredient> _children = new List<IIngredient>();
+
+        public void Add(IIngredient component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(IIngredient component)
+        {
+            _children.Remove(component);
+        }
+
+        public IIngredient[] GetChildren()
+        {
+            return _children.ToArray();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Grupa: {Name}, Ilość: {Quantity}");
+            foreach (var child in _children)
+            {
+                child.Display();
+            }
+        }
+    }
+}
--- MealMap.Domain/Models/Ingredient.cs
+++ MealMap.Domain/Models/Ingredient.cs
@@ -1,10 +1,27 @@
-﻿namespace MealMap.Domain.Models;
+﻿using MealMap.Domain.Interface;
 
-public class Ingredient
+namespace MealMap.Domain.Models;
+
+public class Ingredient : IIngredient
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; }
     public string Category { get; set; } // np. Warzywa i owoce, Nabiał
     public double Quantity { get; set; }
     public string Unit { get; set; } // np. gramy, litry
+
+    public void Display()
+    {
+        Console.WriteLine($"Składnik: {Name} - {Quantity} {Unit}");
+    }
+
+    public void EditUnit(string unit)
+    {
+        Unit = unit;
+    }
+
+    public void EditQuantity(double quantity)
+    {
+        Quantity = (int)quantity;
+    }
 }
