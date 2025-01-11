68fcd921c57d999c67603e7786d9db91822b5222
Divel5577 <gitpdulkowski@gmail.com>
2025-01-11 01:04:30 +0100

    add Singleton

---
+++ MealMap.Domain/MealDatabase.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using MealMap.Domain.Models;
+
+namespace MealMap.Domain
+{
+    public class MealDatabase
+    {
+        private static MealDatabase _instance;
+        private static readonly object LockObject = new();
+
+        public List<Recipe> Recipes { get; private set; }
+        public List<MealPlan> MealPlans { get; private set; }
+
+        private MealDatabase()
+        {
+            Recipes = new List<Recipe>();
+            MealPlans = new List<MealPlan>();
+        }
+
+        public static MealDatabase Instance()
+        {
+            if (_instance == null)
+            {
+                lock (LockObject)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MealDatabase();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            if (recipe != null)
+            {
+                Recipes.Add(recipe);
+            }
+        }
+
+        public void AddMealPlan(MealPlan mealPlan)
+        {
+            if (mealPlan != null)
+            {
+                MealPlans.Add(mealPlan);
+            }
+        }
+
+
+
+        // Metody testowe do wyświetlania przepisów i planów
+        public void DisplayRecipes()
+        {
+            foreach (var recipe in Recipes)
+            {
+                Console.WriteLine($"Recipe: {recipe.Name}, Category: {recipe.Category}, Calories: {recipe.Calories}, Instructions: {recipe.Instructions}");
+                foreach (var ingredient in recipe.Ingredients)
+                {
+                    Console.WriteLine($"  Ingredient: {ingredient.Name}, Quantity: {ingredient.Quantity} {ingredient.Unit}");
+                }
+            }
+        }
+        public void DisplayMealPlans()
+        {
+            foreach (var mealPlan in MealPlans)
+            {
+                Console.WriteLine($"Meal Plan: {mealPlan.PlanName}, Start Date: {mealPlan.StartDate}, End Date: {mealPlan.EndDate}");
+                foreach (var meal in mealPlan.Meals)
+                {
+                    Console.WriteLine($"  Meal: {meal.Name}, Date: {meal.Date}, Time of Day: {meal.TimeOfDay}");
+                    foreach (var recipe in meal.Recipes)
+                    {
+                        Console.WriteLine($"    Recipe: {recipe.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
--- MealMap/Program.cs
+++ MealMap/Program.cs
@@ -1,6 +1,78 @@
 ﻿using MealMap.Application.ShoppingListExporter.Exporters;
 using MealMap.Application.ShoppingListExporter;
 using MealMap.Domain.Models;
+using MealMap.Domain;
+
+
+var database = MealDatabase.Instance();
+
+// Dodawanie danych do bazy
+var saladRecipe = new Recipe
+{
+    Name = "Sałatka a'la caprese",
+    Ingredients = new List<Ingredient>
+            {
+                new() { Name = "Pomidor", Quantity = 3, Unit = "szt.", Category = "Warzywa" },
+                new() { Name = "Mozzarella", Quantity = 125, Unit = "g", Category = "Sery" }
+            },
+    Category = "Śniadanie",
+    Instructions = "Pokrój składniki i ułóż naprzemiennie",
+    Calories = 350
+};
+database.AddRecipe(saladRecipe);
+
+var toastRecipe = new Recipe
+{
+    Name = "Tost z masłem",
+    Ingredients = new List<Ingredient>
+    {
+        new() { Name = "Chleb tostowy", Quantity = 2, Unit = "szt.", Category = "Pieczywo" },
+        new() { Name = "Masło", Quantity = 20, Unit = "g", Category = "Nabiał" }
+    },
+    Category = "Śniadanie",
+    Instructions = "Opiecz kromki chleba, a następnie posmaruj je masłem.",
+    Calories = 200
+};
+database.AddRecipe(toastRecipe);
+
+var lunchRecipe = new Recipe
+{
+    Name = "Spaghetti Bolognese",
+    Ingredients = new List<Ingredient>
+            {
+                new() { Name = "Makaron spaghetti", Quantity = 250, Unit = "g", Category = "Makarony" },
+                new() { Name = "Mięso mielone", Quantity = 300, Unit = "g", Category = "Mięso" },
+                new() { Name = "Sos pomidorowy", Quantity = 200, Unit = "ml", Category = "Przetwory" }
+            },
+    Category = "Obiad",
+    Instructions = "Ugotuj makaron, podsmaż mięso, dodaj sos i wymieszaj.",
+    Calories = 650
+};
+database.AddRecipe(lunchRecipe);
+
+database.AddMealPlan(new MealPlan
+{
+    PlanName = "Plan Tygodniowy",
+    StartDate = DateTime.Now,
+    EndDate = DateTime.Now.AddDays(7),
+    Meals = new List<Meal>
+            {
+                new Meal
+                {
+                    Name = "Śniadanie",
+                    Date = DateTime.Now,
+                    TimeOfDay = "Rano",
+                    Recipes = database.Recipes.Where(r => r.Name == "Sałatka a'la caprese" || r.Name == "Tost z masłem").ToList()
+                },
+                new Meal
+                {
+                    Name = "Obiad",
+                    Date = DateTime.Now,
+                    TimeOfDay = "Południe",
+                    Recipes = database.Recipes.Where(r => r.Name == "Spaghetti Bolognese").ToList()
+                }
+            }
+});
 
 var ingredients = new List<Ingredient>
 {
@@ -13,3 +85,6 @@ exporter.Export(ingredients);
 
 exporter.SetStrategy(new JsonExportStrategy());
 exporter.Export(ingredients);
+
+database.DisplayRecipes();
+database.DisplayMealPlans();
